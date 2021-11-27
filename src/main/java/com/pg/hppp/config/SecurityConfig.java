fa4021f80e3fc1a21e476904d31eed3b5e7aca8a
Filip Jozwiakowski <filip-jozwiakowski@wp.pl>
2021-11-28 00:56:45 +0100

    Adding missing Classes.

---
+++ src/main/java/com/pg/hppp/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.pg.hppp.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorize -> authorize
+                        .antMatchers("/h2-console/**").permitAll() // for H2 console free access
+                        .antMatchers("/", "/register", "/webjars/**", "/login", "/resources/**").permitAll()
+                        .antMatchers("/materials").permitAll())
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin(httpSecurityFormLoginConfigurer -> httpSecurityFormLoginConfigurer
+                        .loginProcessingUrl("/login")
+                        .loginPage("/login").permitAll()
+                        .successForwardUrl("/")
+                        .defaultSuccessUrl("/")
+                        .failureUrl("/login?error"))
+                .logout(httpSecurityLogoutConfigurer -> httpSecurityLogoutConfigurer
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout","GET"))
+                        .logoutSuccessUrl("/?success")
+                        .permitAll())
+                .httpBasic()
+                .and()
+                .csrf().disable();
+
+        // H2 console frame config
+        http.headers().frameOptions().sameOrigin();
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web
+                .ignoring()
+                .antMatchers("/assets/**");
+    }
+}
---
+++ src/main/java/com/pg/hppp/controllers/LineController.java
@@ -0,0 +1,53 @@
+package com.pg.hppp.controllers;
+
+import com.pg.hppp.model.Line;
+import com.pg.hppp.model.User;
+import com.pg.hppp.repositories.UserRepository;
+import com.pg.hppp.services.LineService;
+import com.pg.hppp.services.MaterialFormFilter;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+import java.util.Set;
+
+@AllArgsConstructor
+@Controller
+public class LineController {
+
+    private final LineService lineService;
+    private final UserRepository userRepository;
+
+    public User getTestUser(Integer userId) {
+        return userRepository.findById(userId).orElse(null); //todo replace with authUser after testing
+    }
+
+    @GetMapping("/materials")
+    public String showMaterials(MaterialFormFilter lineFilterDefault, Model model, @AuthenticationPrincipal User authUser) {
+        model.addAttribute("filter", lineFilterDefault.builder().build());
+
+        Set<Line> lines = lineService.getAllLinesForAuthUser(getTestUser(1));
+        model.addAttribute("lines", lines);
+
+        return "line/materials";
+    }
+
+    @PostMapping("/materials")
+    public String filterMaterials(MaterialFormFilter lineFilterForm, Model model, @AuthenticationPrincipal User authUser) {
+        model.addAttribute("filter", lineFilterForm);
+
+        Set<Line> lines = lineService.getAllLinesFilteredForAuthUser(getTestUser(1),lineFilterForm);
+        model.addAttribute("lines", lines);
+
+        return "line/materials";
+    }
+
+    @GetMapping("/risks")
+    public String showRisks() {
+        return "line/risks";
+    }
+}
